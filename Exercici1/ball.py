9fa0f623223c7a61c81948c1e63db67e11dc5162
Ricard <ricard.colet@students.salle.url.edu>
2025-02-14 15:08:56 +0100

    Commit inici

---
+++ Exercici1/ball.py
@@ -0,0 +1,31 @@
+import pygame
+import numpy as np
+
+class Ball:
+    def __init__ (self, radius, mass, pos_initial):
+        self.radius = radius
+        self.mass = mass
+        self.pos = np.array(pos_initial)
+        self.vel = np.array(np.zeros(2))
+        self.acc = np.array(np.zeros(2))
+
+
+    def apply_force(self, dt):
+        #self.acc += force / self.mass
+     
+        self.acc = self.acc + np.array([0, 9.81]) / dt #gravity
+
+    def update(self):
+        self.vel = self.vel + self.acc
+        self.pos = self.pos + self.vel + 0.5 * self.acc
+        self.acc = np.array([0, 0])
+        if (self.pos[1] > 600 - self.radius):
+            self.vel = 0.99 * self.vel
+            self.vel[1] = -self.vel[1]
+
+        if (self.pos[1] > 600 - self.radius):
+            self.pos[1] = 600 - self.radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.pos, self.radius)
+        
---
+++ Exercici1/main.py
@@ -0,0 +1,37 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+import numpy as np
+from ball import Ball
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+
+frames = 60
+#Our setup Create a ball
+ball = Ball(10, 10, [400, 30])
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
+    ball.apply_force()
+    ball.update(frames)
+    ball.draw(screen)
+    
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
