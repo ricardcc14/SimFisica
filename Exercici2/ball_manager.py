4d29692ec3261889f6565439b5ad8ea0316c899d
alexaloonso <alex.aa@students.salle.url.edu>
2025-02-19 09:55:18 +0100

    Ex1 modificat i inici ex2

---
+++ Exercici2/ball_manager.py
@@ -0,0 +1,31 @@
+import pygame
+import numpy as np
+from ball import Ball
+
+class Ball_Manager:
+
+    def __init__ (self, radius, mass):
+        self.balls = []
+        self.balls_radius = radius
+        self.balls_mass = mass
+
+    def addBallWithAppliedForce(self, mouse_start_pos, mouse_end_pos):
+        #Create new ball object
+        ball = Ball(self.balls_radius, self.balls_mass, mouse_end_pos)
+
+        #Calculate applied force 
+        force_vector = np.abs(np.array(mouse_end_pos) - np.array(mouse_start_pos))
+
+        #Apply new force
+        ball.apply_force(force_vector)
+
+        #Save ball
+        self.balls.append(ball)
+
+    def renderAllBalls(self, dt, screen):
+        for ball in self.balls:
+            ball.apply_force([0, 0])
+            ball.update(dt)
+            ball.draw(screen)
+
+
--- Exercici2/main.py
+++ Exercici2/main.py
@@ -2,6 +2,8 @@
 import pygame
 import numpy as np
 from ball import Ball
+from ball_manager import Ball_Manager
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -9,8 +11,8 @@ clock = pygame.time.Clock()
 running = True
 frames = 60
 
-#Our setup Create a ball
-ball = Ball(50, 100, [400, 300])
+#Our setup: Create a ball manager
+ball_manager = Ball_Manager(10, 50)
 
 while running:
     # poll for events
@@ -18,19 +20,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_start_pos = event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse_end_pos = event.pos
+            ball_manager.addBallWithAppliedForce(mouse_start_pos, mouse_end_pos)
+
+
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
     dt = clock.tick(frames) / 1000
 
     # RENDER YOUR GAME HERE
-    ball.apply_force([0,0])
-    ball.update(dt)
-    ball.draw(screen)
+    ball_manager.renderAllBalls(dt, screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(frames) 
-
-pygame.quit()
