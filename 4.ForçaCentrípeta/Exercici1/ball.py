8ed20c110e7f0c41f86a3d04bd61ff35a1d26c95
Ricard <ricard.colet@students.salle.url.edu>
2025-03-02 13:07:34 +0100

    Modificacions moviment angular + Exercici1

---
+++ 4.ForçaCentrípeta/Exercici1/ball.py
@@ -0,0 +1,90 @@
+import pygame
+import numpy as np
+
+class Ball:
+    def __init__ (self, radius, mass, pos_initial, color):
+        self.radius = radius
+        self.mass = mass
+        self.color = color
+        self.pos = np.array(pos_initial)
+        self.vel = np.array(np.zeros(2))
+        self.acc = np.array(np.zeros(2))
+        self.gravity = np.array([0, -9.81])
+
+        #Paràmetres pel moviment angular
+        self.radius = 100
+        self.theta = 0.0
+        self.angular_vel = 0.0
+        self.angular_acc = 0.01
+
+    def apply_force(self, force):     
+        self.acc = self.acc + (np.array(force) / self.mass)
+
+    def update(self, dt):
+        #self.acc = self.acc + self.gravity
+        #self.vel = self.vel + self.acc * dt
+        #self.pos = self.pos + self.vel * dt + 0.5 * (self.acc * np.power(dt,2))
+        self.angular_vel += self.angular_acc * dt
+        if self.angular_vel > (4 * np.pi):
+            self.angular_vel = 4 * np.pi
+        self.theta += self.angular_vel * dt
+        self.pos = np.array([self.radius * np.sin(self.theta) + 400 , self.radius * np.cos(self.theta) + 300])
+        
+
+
+    def get_kinetic_energy(self): 
+        energy = np.array([0, 0])
+        energy = 0.5 * self.mass * np.pow(self.vel,2)
+        return energy   
+
+    def get_potential_energy(self, screen:pygame.Surface):
+        energy = np.array([0, 0])
+        energy = self.mass * self.gravity * (screen.get_height() - self.pos)
+        return energy
+        
+    def collision_ball(self, other_ball):
+
+        #Trobar els vectors unitaris de l'eix de xoc
+        u = np.array(other_ball.pos - self.pos) / np.linalg.norm(other_ball.pos - self.pos)
+        w = np.array([-u[1], u[0]])
+
+        #Distància de seguretat
+        if (np.abs(np.linalg.norm(other_ball.pos - self.pos)) <= (self.radius + other_ball.radius)):
+            self.pos = self.pos + 1.1*(-u * ((self.radius + other_ball.radius) - np.linalg.norm(other_ball.pos - self.pos)))
+
+        #Calcular velocitats finals amb nova base
+        x_1 = ((self.mass - other_ball.mass) / (self.mass + other_ball.mass)) * self.vel + ((2 * other_ball.mass)/(self.mass + other_ball.mass)) * other_ball.vel
+        x_2 =  ((2 * self.mass) / (self.mass + other_ball.mass)) * self.vel + ((other_ball.mass - self.mass) / (self.mass + other_ball.mass)) * other_ball.vel
+        
+        self.vel = x_1 * u +  self.vel * w
+        other_ball.vel = x_2 * u + other_ball.vel * w
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+
+    def checkScreenEdges(self, screen:pygame.Surface):
+        #Bottom edge
+        if (self.pos[1] > screen.get_height() - self.radius):
+            self.vel[1] = -self.vel[1]
+            self.pos[1] = screen.get_height() - self.radius
+
+        #Top edge
+        if (self.pos[1] < 0 + self.radius):
+            self.vel[1] = -self.vel[1]
+            self.pos[1] = self.radius
+
+        #Left edge
+        if (self.pos[0] < 0 + self.radius):
+            self.vel[0] = -self.vel[0]
+            self.pos[0] = self.radius
+
+        #Right edge
+        if (self.pos[0] > screen.get_width() - self.radius):
+            self.vel[0] = -self.vel[0]
+            self.pos[0] = screen.get_width() - self.radius
+
+    def apply_normal_and_friction_force(self, inclination, coef):
+        N = -(self.gravity[1] * self.mass * np.cos(inclination))
+        self.apply_force([-coef * N * -self.vel[0], -coef * N * -self.vel[1]])
+        pass
+    
---
+++ 4.ForçaCentrípeta/Exercici1/main.py
@@ -0,0 +1,50 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+import numpy as np
+from ball import Ball
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+frames = 60
+factor = 5
+render = False
+
+#Our setup 
+
+# Títol
+font = pygame.font.SysFont("Arial", 20)
+ui_title = font.render('Press to start!', True, "white")
+
+# Create ball with applied force
+ball = Ball(25, 10, np.array([400, 300]), 'white')
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("teal")
+
+    # RENDER YOUR GAME HERE
+    ball.checkScreenEdges(screen)
+    ball.update(factor/frames)
+    ball.draw(screen)
+
+
+    
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(frames) 
+
+pygame.quit()
