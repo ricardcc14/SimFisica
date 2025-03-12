a166fb1114f35f25421f563723c3001eaef84ab6
Ricard <ricard.colet@students.salle.url.edu>
2025-03-12 11:25:18 +0100

    Exercici 2 + Exercici 3 (falta acabar) Exerici1 està malament s'ha de posar el de prova

---
+++ 5.ForcesComplexes/Exercici2/ball.py
@@ -0,0 +1,61 @@
+import pygame
+import numpy as np
+
+class Ball:
+    def __init__ (self, radius, mass, pos_initial, vel_initial):
+        self.radius = radius
+        self.mass = mass
+        self.pos = np.array(pos_initial)
+        self.vel = vel_initial
+        self.acc = np.array(np.zeros(2))
+        #self.gravity = np.array([0, 9.81])
+
+    def apply_force(self, force):     
+        self.acc = self.acc + (np.array(force) / self.mass)
+
+    def universal_gravity(self, balls):
+        G = 6.67 * 10**-11
+        total_force = np.array([0, 0])
+        for ball in balls:
+            if (ball != self):
+                direction = ball.pos - self.pos
+                distance = np.linalg.norm(direction)
+                if (distance > self.radius + ball.radius):
+                    force = (G * self.mass * ball.mass)/ (distance ** 2)
+                    print("acc: ", force)
+                    force_dir = direction / distance
+                    total_force = total_force + force * force_dir
+        
+        self.apply_force(total_force)
+               
+
+    def update(self, dt):
+        
+        #self.acc = self.acc + self.gravity
+        self.vel = self.vel + self.acc * dt
+        self.pos = self.pos + self.vel * dt # + 0.5 * (self.acc * np.power(dt,2))
+        self.acc = np.array([0, 0])
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.pos, self.radius)
+
+    def checkScreenEdges(self, screen:pygame.Surface):
+        #Bottom edge
+        if (self.pos[1] > screen.get_height() - self.radius):
+            self.vel[1] = -self.vel[1]
+            self.pos[1] = screen.get_height() - self.radius
+
+        #Top edge
+        if (self.pos[1] < 0 + self.radius):
+            self.vel[1] = -self.vel[1]
+            self.pos[1] = self.radius
+
+        #Left edge
+        if (self.pos[0] < 0 + self.radius):
+            self.vel[0] = -self.vel[0]
+            self.pos[0] = self.radius
+
+        #Right edge
+        if (self.pos[0] > screen.get_width() - self.radius):
+            self.vel[0] = -self.vel[0]
+            self.pos[0] = screen.get_width() - self.radius
---
+++ 5.ForcesComplexes/Exercici2/main.py
@@ -0,0 +1,47 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+import numpy as np
+from ball import Ball
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+frames = 60
+factor = 10
+
+#Our setup Create a ball
+center_ball = Ball(10, np.power(10, 14), [400, 300], [0, 0])
+center_ball.apply_force([0,0])
+
+ball1 = Ball(20, 10, [150, 200], [4, 1])
+ball2 = Ball(20, 10, [650, 200], [-2, 3])
+ball3 = Ball(20, 10, [450, 100], [-5, 2])
+ball4 = Ball(20, 10, [250, 550], [1, -2])
+
+balls = [center_ball, ball1, ball2, ball3, ball4]
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
+    for ball in balls:
+        ball.universal_gravity(balls)
+        ball.checkScreenEdges(screen)
+        ball.update(factor/frames)
+        ball.draw(screen)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(frames) 
+
+pygame.quit()
