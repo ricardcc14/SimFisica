db93b57ec6d07f4ecf6c2bad6d67ebc887544a7b
Ricard <ricard.colet@students.salle.url.edu>
2025-04-09 18:23:09 +0200

    Propietats diferents blocs.

--- Pràctica1/AngryBirds/GlassBox.py
+++ Pràctica1/AngryBirds/GlassBox.py
@@ -8,3 +8,17 @@ class GlassBox(Box):
         self.images = images
         super().__init__(world, x, y, w, h, images)
 
+        self.body.DestroyFixture(self.body.fixtures[0])
+
+        # Crear un nou fixture amb la densitat modificada
+        boxShape = b2.b2PolygonShape()
+
+        boxShape = b2.b2PolygonShape(box=utils.pixelToWorld(w/2, h/2))
+
+        fd = b2.b2FixtureDef()
+        fd.shape = boxShape
+        fd.density = 1  # Densitat modificada
+        fd.restitution = 0.2
+        fd.friction = 0.01
+        self.body.CreateFixture(fd)
+
--- Pràctica1/AngryBirds/StoneBox.py
+++ Pràctica1/AngryBirds/StoneBox.py
@@ -7,3 +7,17 @@ class StoneBox(Box):
     def __init__(self, world, x, y, w, h, images:list[pygame.Surface]):
         self.images = images
         super().__init__(world, x, y, w, h, images)
+
+        self.body.DestroyFixture(self.body.fixtures[0])
+
+        # Crear un nou fixture amb la densitat modificada
+        boxShape = b2.b2PolygonShape()
+
+        boxShape = b2.b2PolygonShape(box=utils.pixelToWorld(w/2, h/2))
+
+        fd = b2.b2FixtureDef()
+        fd.shape = boxShape
+        fd.density = 8  # Densitat modificada
+        fd.restitution = 0.5
+        fd.friction = 0.3
+        self.body.CreateFixture(fd)
