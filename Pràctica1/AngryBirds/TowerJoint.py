bdb8bda30d5c6dc2d5ffe1560fa02f368894be94
ricardcc <ricardcc04@gmail.com>
2025-04-10 12:30:33 +0200

    Joints acabats falta implementar-los des del Load del levels.json.

--- Pràctica1/AngryBirds/TowerJoint.py
+++ Pràctica1/AngryBirds/TowerJoint.py
@@ -10,7 +10,7 @@ class TowerJoint():
     def __init__(self, world, x, y, w, h, material):
         self.material = material
         self.images = []
-        self.boxes = []
+        self.boxes:list[Box] = []
         self.currentStatus = 0
         self.images.append(pygame.image.load("assets/blocks/" + material + "/box/1.png"))
         self.images.append(pygame.image.load("assets/blocks/" + material + "/box/2.png"))
@@ -22,23 +22,29 @@ class TowerJoint():
             if material == "wood":
                 self.boxes.append(WoodBox(world, x, ypos, w, h, self.images))
             elif material == "stone":
-                self.boxes.append(StoneBox(world, x, ypos, w, h, self.images))
+                self.boxes.append(StoneBox(world, x, y, w, h, self.images))
             elif material == "glass":
-                self.boxes.append(GlassBox(world, x, ypos, w, h, self.images))   
+                self.boxes.append(GlassBox(world, x, y, w, h, self.images))   
         
-
+        print("BOXES:", self.boxes[-1].body.position, self.boxes[0].body.position, self.boxes[1].body.position)
         self.joints = []
         for i in range(len(self.boxes) - 1):
             joint = world.CreateWeldJoint(
                 bodyA=self.boxes[i].body,
                 bodyB=self.boxes[i + 1].body,
-                localAnchorA=(0, h / 2),
-                localAnchorB=(0, -h / 2),
-                collideConnected=False
+                localAnchorA=(0, (h/100) / 2),
+                localAnchorB=(0, (-h/100) / 2),
+                collideConnected=True
             )
             self.joints.append(joint)
 
     def draw(self, screen:pygame.Surface):
-        for box in self.boxes:
+        for i, box in enumerate(self.boxes):
+            #print("BOX DRAW:", i, "POS: ", box.body.position)
             box.draw(screen)
+
+    def update(self,world):
+        for box in self.boxes:
+            box.update(world)
+        
 
